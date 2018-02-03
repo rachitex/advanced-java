7c39b8927e1ec94977960fab55dac88a6bd5be40
moon <rachitmishra5594@gmail.com>
2018-02-03 23:33:10 +0530

    Created new Project Student Management

---
+++ StudentManagement/src/studentmanagement/StudentManagement.java
@@ -0,0 +1,12 @@
+package studentmanagement;
+
+import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class StudentManagement {
+    public static void main(String[] args) {
+        
+    } 
+}
