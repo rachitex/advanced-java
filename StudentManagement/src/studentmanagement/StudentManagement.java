9a8eb83bc74a231a99f1d8d1a752bc497bdbafa2
moon <rachitmishra5594@gmail.com>
2018-02-03 23:37:15 +0530

    Added Variables in StudentManagement.java

--- StudentManagement/src/studentmanagement/StudentManagement.java
+++ StudentManagement/src/studentmanagement/StudentManagement.java
@@ -6,7 +6,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class StudentManagement {
+    
+    Connection con = null;
+    String url = "jdbc:mysql://localhost:3306/";
+    String db = "student";
+    String driver = "com.mysql.jdbc.Driver";
+    
     public static void main(String[] args) {
-        
     } 
 }
