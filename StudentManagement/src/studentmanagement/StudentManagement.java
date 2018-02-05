a7ca8ee631bfeeac082ecd98da8f9a8411be07aa
moon <rachitmishra5594@gmail.com>
2018-02-05 13:08:29 +0530

    Added UserLogin Project

--- StudentManagement/src/studentmanagement/StudentManagement.java
+++ StudentManagement/src/studentmanagement/StudentManagement.java
@@ -8,10 +8,10 @@ import java.sql.Statement;
 
 public class StudentManagement {
     
-    private static Connection con = null;
-    private static String url = "jdbc:mysql://localhost:3306/";
-    private static String db = "student";
-    private static String driver = "com.mysql.jdbc.Driver";
+    Connection con = null;
+    private static final String url = "jdbc:mysql://localhost:3306/";
+    private static final String db = "student";
+    private static final String driver = "com.mysql.jdbc.Driver";
     
     StudentManagement(){
         try{
@@ -30,16 +30,43 @@ public class StudentManagement {
     
     public static void main(String[] args) {
             new StudentManagement();
-            try{
-                con.setAutoCommit(false);
-                Statement stmt = con.createStatement();
-                String createQuery = "drop table student";
-                stmt.execute(createQuery);
-                System.out.println("Table deleted successfully!!!");
-            }catch(SQLException e){
-                e.printStackTrace();
-            }catch(Exception e){
-                e.printStackTrace();
-            }
+            Connection con = null;
+            Statement stmt = null;
+            Scanner scan = new Scanner(System.in);
+            int choice = 0;
+            do{
+                   try{
+                //con.setAutoCommit(false);
+                stmt = con.createStatement();
+                System.out.println("Enter the choice to perform: ");
+                System.out.println("Press 1 to perform Insert operation.");
+                System.out.println("Press 2 to perform Read operation.");
+                System.out.println("Press 3 to perform Update operation.");
+                System.out.println("Press 4 to perform Delete operation.");
+            
+                try{
+                    choice = scan.nextInt();
+                }catch(Exception e){
+                    scan.next();
+                }
+                
+                switch(choice){
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    default:
+                        System.out.println("Enter a valid choice!!!");
+                }
+                stmt.close();
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }while(choice != -9999);
+            scan.close();
     } 
 }
---
+++ UserLogin/src/userlogin/UserLogin.java
@@ -0,0 +1,56 @@
+package userlogin;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class UserLogin {
+    
+    static Connection con = null;
+    private static final String url = "jdbc:mysql://localhost:3306/";
+    private static final String db = "login";
+    private static final String driver = "com.mysql.jdbc.Driver";
+    
+    UserLogin(){
+        try{
+            Class.forName(driver);
+            System.out.println("Driver Loaded...");
+            con = DriverManager.getConnection(url+db, "root", "password123!");
+            System.out.println("Connection Established..."); 
+        }catch(ClassNotFoundException e){
+        }catch(SQLException e){
+        }catch(Exception e){
+        }
+    }
+    
+    public static void main(String[] args) {
+        new UserLogin();
+        Statement stmt = null;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter User ID: ");
+        String userid = scan.next();
+        System.out.print("Enter Password: ");
+        String password = scan.next();
+        String selectQuery = "select * from login where id = '"+userid+"'AND "
+                + "pass = '"+password+"';";
+        try{
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(selectQuery);
+            if(rs.next()){
+                System.out.println("Successful Login!!!");
+            }else{
+                System.out.println("Invalid Username or Password!!!");
+            }
+            rs.close();
+            stmt.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }catch(Exception e){
+        }
+        
+    }
+    
+}
