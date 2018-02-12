eead0db3b8c3288f8e9161716824d86062f66d7f
moon <rachitmishra5594@gmail.com>
2018-02-12 08:58:29 +0530

    Added Update and Delete functionality in InsertUpdateDelete

--- InsertUpdateDelete/src/insertupdatedelete/InsertUpdateDelete.java
+++ InsertUpdateDelete/src/insertupdatedelete/InsertUpdateDelete.java
@@ -32,7 +32,6 @@ public class InsertUpdateDelete {
     public static void main(String[] args) {
            new InsertUpdateDelete();
            try{
-                stmt = con.createStatement();
                 Scanner scan = new Scanner(System.in);
                 int choice = 0;
                 do{
@@ -57,14 +56,113 @@ public class InsertUpdateDelete {
                                     + " values"
                                     + "("+studentId+", '"+studentName+"', '"+course+"'"
                                     + ");";
-                            //stmt = con.createStatement();
+                            stmt = con.createStatement();
                             stmt.executeUpdate(insertQuery);
-                            System.out.println("Rows inserted Successfully");
+                            System.out.println("Rows inserted Successfully\n\n");
                             stmt.close();
                             break;
                         case 2:
+                            System.out.print("Enter ID of the student: ");
+                            int updateId = scan.nextInt();
+                            String oldSelectQuery = "select * from student_info;";
+                            stmt = con.createStatement();
+                            rs = stmt.executeQuery(oldSelectQuery);
+                            System.out.println("\nExisting Records: ");
+                            while(rs.next()){
+                                studentId = rs.getInt("student_id");
+                                studentName = rs.getString("student_name");
+                                course = rs.getString("course");
+                                System.out.println("Student ID: "+studentId);
+                                System.out.println("Student Name: "+studentName);
+                                System.out.println("Course: "+course);
+                            }
+                            rs.close();
+                            //stmt.close();
+                            
+                            System.out.println("\nWhat do you want to Update?");
+                            System.out.println("Press 1 for Name.");
+                            System.out.println("Press 2 for Course.");
+                            System.out.println("Press 3 for exit.");
+                            int choice2 = scan.nextInt();
+                            String updateQuery = "";
+                            switch(choice2){
+                                case 1:
+                                    System.out.print("Enter new Name: ");
+                                    studentName = scan.next();
+                                    updateQuery = "Update student_info "
+                                            + "set student_name = '"+studentName+"'"
+                                            + " where student_id = "+updateId;
+                                    stmt.executeUpdate(updateQuery);
+                                    System.out.println("\nName updated successfully! \n");
+                                    //stmt.close();
+                                    break;
+                                case 2:
+                                    System.out.print("Enter new Course: ");
+                                    course = scan.next();
+                                    updateQuery = "Update student_info "
+                                            + "set course = '"+course+"'"
+                                            + " where student_id = "+updateId;
+                                    stmt.executeUpdate(updateQuery);
+                                    System.out.println("\nCourse updated successfully! \n");
+                                    //stmt.close();
+                                    break;
+                                case 3:
+                                    break;
+                                default:
+                                    System.out.println("Enter valid choice!");
+                            }
+                            String newSelectQuery = "select * from student_info"
+                                    + "where student_id = "+updateId;
+                            rs = stmt.executeQuery(newSelectQuery);
+                            System.out.println("\nNew Records: ");
+                            while(rs.next()){
+                                studentId = rs.getInt("student_id");
+                                studentName = rs.getString("student_name");
+                                course = rs.getString("course");
+                                System.out.println("Student ID: "+studentId);
+                                System.out.println("Student Name: "+studentName);
+                                System.out.println("Course: "+course);
+                            }
+                            rs.close();
+                            stmt.close();
                             break;
                         case 3:
+                            System.out.print("Enter ID of the student: ");
+                            int deleteId = scan.nextInt();
+                            String oldDeleteSelectQuery = "select * from student_info;";
+                            stmt = con.createStatement();
+                            rs = stmt.executeQuery(oldDeleteSelectQuery);
+                            System.out.println("\nExisting Records: ");
+                            while(rs.next()){
+                                studentId = rs.getInt("student_id");
+                                studentName = rs.getString("student_name");
+                                course = rs.getString("course");
+                                System.out.println("Student ID: "+studentId);
+                                System.out.println("Student Name: "+studentName);
+                                System.out.println("Course: "+course);
+                            }
+                            rs.close();
+                            //stmt.close();
+                            
+                            String deleteQuery = "delete from student_info "
+                                    + "where student_id = "+deleteId;
+                            stmt.executeUpdate(deleteQuery);
+                            System.out.println("\nRow Deleted Successfully");
+                            //stmt.close();
+                            
+                            String newDeleteSelectQuery = "select * from student_info;";
+                            rs = stmt.executeQuery(newDeleteSelectQuery);
+                            System.out.println("\nNew Records: ");
+                            while(rs.next()){
+                                studentId = rs.getInt("student_id");
+                                studentName = rs.getString("student_name");
+                                course = rs.getString("course");
+                                System.out.println("Student ID: "+studentId);
+                                System.out.println("Student Name: "+studentName);
+                                System.out.println("Course: "+course);
+                            }
+                            rs.close();
+                            stmt.close();
                             break;
                         default:
                             System.out.println("Invalid Choice!!!");
