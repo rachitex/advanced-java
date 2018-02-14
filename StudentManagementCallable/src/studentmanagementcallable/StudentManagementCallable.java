ec59191ed8a0532f947b5bb82a055ffc4eb05946
moon <rachitmishra5594@gmail.com>
2018-02-15 00:38:22 +0530

    Finished StudentManagementCallable

--- StudentManagementCallable/src/studentmanagementcallable/StudentManagementCallable.java
+++ StudentManagementCallable/src/studentmanagementcallable/StudentManagementCallable.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.CallableStatement;
-import java.sql.ResultSet;
+import java.sql.Types;
 import java.util.Scanner;
 
 public class StudentManagementCallable {
@@ -21,13 +21,40 @@ public class StudentManagementCallable {
             con = DriverManager.getConnection(url+db, "root", "");
             System.out.println("Connection Established..");
         }catch(SQLException e){
+            e.printStackTrace();
         }catch(ClassNotFoundException e){
         }
         
     }
     
     public static void main(String[] args) {
-      
+      new StudentManagementCallable();
+       CallableStatement cstmt = null;
+       Scanner scan = new Scanner(System.in);
+       
+       String callSQL = "{ call calculate_result(?,?,?,?,?) }";
+       try{
+            cstmt = con.prepareCall(callSQL);
+            System.out.print("Enter the student ID: ");
+            int rollNo = scan.nextInt();
+            cstmt.setInt(1, rollNo); //Setting IN parameter
+            cstmt.registerOutParameter(2, Types.VARCHAR); //Setting OUT parameter
+            cstmt.registerOutParameter(3, Types.INTEGER); 
+            cstmt.registerOutParameter(4, Types.DOUBLE);
+            cstmt.registerOutParameter(5, Types.VARCHAR);
+            cstmt.execute();
+            
+            String name = cstmt.getString(2);
+            int total = cstmt.getInt(3);
+            double percentage = cstmt.getDouble(4);
+            String grade = cstmt.getString(5);
+            System.out.println("Result:\n Name = "+name+", Total = "+total
+                    + ", Percentage = "+percentage
+                    +", Grade = "+grade);
+       }catch(SQLException e){
+           e.printStackTrace();
+       }catch(Exception e){
+       }
     }
     
 }
